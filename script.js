7755624ffa41de43bac7356574d71276186b2afb
Mufti Ramdhani <muftiramdhani@gmail.com>
2021-10-21 10:33:23 +0700

    [mufti] add content

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ hamburger.addEventListener("click", function () {
   navigation.classList.toggle("display_block");
 });
 
+/*===== REMOVE MENU MOBILE =====*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.querySelector(".navigation");
+    navMenu.classList.remove('display_block');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
 // animation for anchor
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
